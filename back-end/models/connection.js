7b278de17abc0400ed5b031865742070f5e7487d
Cardoso <lsc.cardosoo@gmail.com>
2020-09-21 16:06:34 -0300

    Inicializar projeto #back

---
+++ back-end/models/connection.js
@@ -0,0 +1,23 @@
+const mysqlx = require('@mysql/xdevapi');
+
+let schema;
+module.exports = () => (
+  schema
+  ? Promise.resolve(schema)
+  : mysqlx
+    .getSession({
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      host: process.env.HOSTNAME,
+      port: 33060,
+      socketPath: '/var/run/mysqld/mysqld.sock',
+    })
+    .then(async (session) => {
+      schema = await session.getSchema('Trybeer');
+      return schema;
+    })
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    })
+);
