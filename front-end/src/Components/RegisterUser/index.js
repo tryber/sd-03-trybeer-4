f80cf43782f6097d1ac3a13521882d38ae8f0879
Cardoso <lsc.cardosoo@gmail.com>
2020-09-22 15:28:46 -0300

    Definir regras de submit para form de cadastro

--- front-end/src/Components/RegisterUser/index.js
+++ front-end/src/Components/RegisterUser/index.js
@@ -1,59 +1,71 @@
 import React, { useState } from 'react';
 import './styles.css';
 
+const beers = require('../../images/beers.png');
+
 const RegisterUser = () => {
   const [form, setForm] = useState({ name: '', email: '', password: '' });
   const { email, password, name } = form;
 
-  const handleName = ({ target: { value } }) => {
+  const handleInput = (e) => {
     const nameLength = 12;
-    const input = value.length > nameLength ? value : '';
-    return setForm((currentState) => ({ ...currentState, name: input }));
-  };
-
-  const handleEmail = ({ target: { email: value } }) => {
-    const regExr = /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
-    const input = regExr.test(value) ? value : '';
-    return setForm((currentState) => ({ ...currentState, email: input }));
-  };
-
-  const handlePassword = ({ target: { value } }) => {
     const passwordLength = 6;
-    const input = value.length > passwordLength ? value : '';
-    return setForm((currentState) => ({ ...currentState, password: input }));
+    const regExrEmail = /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
+    const { name: userName, value } = e.target;
+    let input = '';
+    if (userName === 'name') {
+      input = value.length >= nameLength ? value : '';
+    }
+    if (userName === 'email') {
+      input = regExrEmail.test(value) ? value : '';
+    }
+    if (userName === 'password') {
+      input = value.length >= passwordLength ? value : '';
+    }
+    return setForm({ ...form, [userName]: input });
   };
 
   return (
-    <section className="register-contrainer">
-      <form action="/register" method="POST">
+    <section className="register-container default-color shadow">
+      <img src={ beers } alt="Icone de duas cervejas" width="100px" />
+      <form action="http://localhost:3001/register" method="POST">
         <label htmlFor="userName">
           Nome
-          <input 
-            onChange={ (e) => handleName(e) }
-            type="text" name="name" id="userName" data-testid="signup-name"
-            />
+          <input
+            onChange={ (e) => handleInput(e) }
+            type="text"
+            name="name"
+            id="userName"
+            data-testid="signup-name"
+          />
         </label>
         <br />
         <label htmlFor="userEmail">
           Email
           <input
-            onChange={ (e) => handleEmail(e) }
-            type="email" name="email" id="userEmail" data-testid="signup-email"
+            onChange={ (e) => handleInput(e) }
+            type="email"
+            name="email"
+            id="userEmail"
+            data-testid="signup-email"
           />
         </label>
         <br />
         <label htmlFor="userPassword">
           Senha
           <input
-            onChange={ (e) => handlePassword(e) }
-            type="password" name="password" id="userPassword"
-            minLength="6" data-testid="signup-password"
+            onChange={ (e) => handleInput(e) }
+            type="password"
+            name="password"
+            id="userPassword"
+            minLength="6"
+            data-testid="signup-password"
           />
         </label>
         <br />
         <label htmlFor="userSeller">
           Quero vender
-          <input type="checkbox" name="seller" id="userSeller" data-testid="signup-seller"/>
+          <input type="checkbox" name="seller" id="userSeller" data-testid="signup-seller" />
         </label>
         <br />
         <button
