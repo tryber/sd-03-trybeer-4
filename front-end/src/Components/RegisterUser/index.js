4b49a572eac08250a9d1a6361919e42dea4027bb
Cardoso <lsc.cardosoo@gmail.com>
2020-09-22 17:52:14 -0300

    Criar menu bar com titulo dinamico para as paginas

--- front-end/src/Components/RegisterUser/index.js
+++ front-end/src/Components/RegisterUser/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import MenuBar from '../MenuBar/index';
 import './styles.css';
 
 const beers = require('../../images/beers.png');
@@ -26,57 +27,61 @@ const RegisterUser = () => {
   };
 
   return (
-    <section className="register-container default-color shadow">
-      <img src={ beers } alt="Icone de duas cervejas" width="100px" />
-      <form action="http://localhost:3001/register" method="POST">
-        <label htmlFor="userName">
-          Nome
-          <input
-            onChange={ (e) => handleInput(e) }
-            type="text"
-            name="name"
-            id="userName"
-            data-testid="signup-name"
-          />
-        </label>
-        <br />
-        <label htmlFor="userEmail">
-          Email
-          <input
-            onChange={ (e) => handleInput(e) }
-            type="email"
-            name="email"
-            id="userEmail"
-            data-testid="signup-email"
-          />
-        </label>
-        <br />
-        <label htmlFor="userPassword">
-          Senha
-          <input
-            onChange={ (e) => handleInput(e) }
-            type="password"
-            name="password"
-            id="userPassword"
-            minLength="6"
-            data-testid="signup-password"
-          />
-        </label>
-        <br />
-        <label htmlFor="userSeller">
-          Quero vender
-          <input type="checkbox" name="seller" id="userSeller" data-testid="signup-seller" />
-        </label>
-        <br />
-        <button
-          disabled={ email === '' || password === '' || name === '' }
-          type="submit"
-          data-testid="signup-btn"
-        >
-          Cadastrar
-        </button>
-      </form>
-    </section>
+    <>
+      <MenuBar titleName="TryBeer" />
+      <section className="register-container default-color shadow">
+        <img src={ beers } alt="Icone de duas cervejas" width="100px" />
+        <form action="http://localhost:3001/register" method="POST">
+          <label htmlFor="userName">
+            Nome
+            <input
+              onChange={ (e) => handleInput(e) }
+              type="text"
+              name="name"
+              id="userName"
+              data-testid="signup-name"
+            />
+          </label>
+          <br />
+          <label htmlFor="userEmail">
+            Email
+            <input
+              onChange={ (e) => handleInput(e) }
+              type="email"
+              name="email"
+              id="userEmail"
+              data-testid="signup-email"
+            />
+          </label>
+          <br />
+          <label htmlFor="userPassword">
+            Senha
+            <input
+              onChange={ (e) => handleInput(e) }
+              type="password"
+              name="password"
+              id="userPassword"
+              minLength="6"
+              data-testid="signup-password"
+            />
+          </label>
+          <br />
+          <label htmlFor="userSeller">
+            Quero vender
+            <input type="checkbox" name="seller" id="userSeller" data-testid="signup-seller" />
+          </label>
+          <br />
+          <button
+            className="confirm-btn"
+            disabled={ email === '' || password === '' || name === '' }
+            type="submit"
+            data-testid="signup-btn"
+          >
+            Cadastrar
+          </button>
+        </form>
+      </section>
+    </>
   );
 };
 
