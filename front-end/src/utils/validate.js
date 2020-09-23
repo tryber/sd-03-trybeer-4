6387d8a112f70f9be1acf30f4a5d437f4da642e5
Cardoso <lsc.cardosoo@gmail.com>
2020-09-23 17:02:51 -0300

    Testar se name nao possui caracteres especiais ou numeros

--- front-end/src/utils/validate.js
+++ front-end/src/utils/validate.js
@@ -1,10 +1,11 @@
 export default (name, value) => {
   const nameLength = 12;
+  const nameType = /^[a-zA-Z]*$/;
   const passwordLength = 6;
   const regExrEmail = /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
   switch (true) {
     case name === 'name':
-      return value.length >= nameLength;
+      return value.length >= nameLength && nameType.test(value);
     case name === 'email':
       return regExrEmail.test(value);
     case name === 'password':
