0cab0ae610465e9da2a73b9cb202407229b2ccec
RobertoAsgon <robertoasgon@gmail.com>
2020-09-23 09:24:33 -0300

    Adicionando middleware para validação do token JWT.

--- back-end/index.js
+++ back-end/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const { registerController } = require('./controllers/userController');
 const { loginController } = require('./controllers/userController');
+const authMiddleware = require('./middlewares/auth');
 
 const app = express();
 
@@ -12,6 +13,9 @@ app.use(bodyParser.json());
 app.get('/', (_req, res) => {
   res.send();
 });
+
+// Rota Get Testando Middleware de Validação do Token JWT
+app.get('/auth', authMiddleware);
 
 app.post('/login', (req, res) => loginController(req, res));
 
