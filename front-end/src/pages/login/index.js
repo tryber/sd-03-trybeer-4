3e84c906b4c779ea10a78572e0cce0153aa70dfb
RobertoAsgon <robertoasgon@gmail.com>
2020-09-23 12:37:29 -0300

    Realizando requisição na Pagina de Login e salvando userData + Token na Storage.

--- front-end/src/pages/login/index.js
+++ front-end/src/pages/login/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import getUserFromAPI from '../../services/api_endpoints';
 import './login.css';
 
 function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const getUserData = async () => {
+    const { data } = await getUserFromAPI(email, password);
+    // console.log('O QUE VEM ? POST REQUEST LOGIN', data);
+    if (data.token) { localStorage.setItem('user', JSON.stringify(data)); }
+    return true;
+  };
 
   return (
     <div className="form">
@@ -13,6 +23,7 @@ function LoginPage() {
           className="text-box"
           name="email"
           data-testid="email-input"
+          onChange={ (event) => setEmail(event.target.value) }
           type="email"
           required
         />
@@ -23,6 +34,7 @@ function LoginPage() {
           id="password"
           className="text-box"
           data-testid="password-input"
+          onChange={ (event) => setPassword(event.target.value) }
           name="password"
           required
           type="password"
@@ -32,6 +44,7 @@ function LoginPage() {
         className="login-btn"
         type="button"
         data-testid="signin-btn"
+        onClick={ () => getUserData(email, password) }
       >
         Entrar
       </button>
