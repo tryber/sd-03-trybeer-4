a574d087e57bac44175224ac26c5689f31363ec3
RobertoAsgon <robertoasgon@gmail.com>
2020-09-23 12:15:18 -0300

    Fix Lint Front-end

--- front-end/src/pages/login/index.js
+++ front-end/src/pages/login/index.js
@@ -1,68 +1,49 @@
 import React, { useState } from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import { getUserFromAPI } from '../../services/api_endpoints';
+import { Link } from 'react-router-dom';
 import './login.css';
 
-const minPasswordSize = 6;
-
 function LoginPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const getUserData = () => {
-    try {
-      const { name, role, token } = getUserFromAPI(email, password);
-      localStorage.setItem('token', token);
-      if (role === 'administrator') return <Redirect to="/admin/profile" />;
-      if (role === 'client') return <Redirect to="/client/products" />;
-      console.log(`Hello, ${name}`);
-    } catch (error) {
-      return error;
-    }
-  };
 
   return (
     <div className="form">
-      <form action="http://localhost:3001/login" method="POST">
-        <label htmlFor="userEmail">Email
-          <input
-            className="text-box"
-            id="userEmail"
-            data-testid="email-input"
-            onChange={ (event) => setEmail(event.target.value) }
-            type="email"
-            required
-          />
-        </label>
-        <label htmlFor="userPass">Senha
-          <input
-            className="text-box"
-            id="userPass"
-            data-testid="password-input"
-            onChange={ (event) => setPassword(event.target.value) }
-            type="password"
-            required
-          />
-        </label>
+      <label htmlFor="email">
+        Email
+        <input
+          id="email"
+          className="text-box"
+          name="email"
+          data-testid="email-input"
+          type="email"
+          required
+        />
+      </label>
+      <label htmlFor="password">
+        Senha
+        <input
+          id="password"
+          className="text-box"
+          data-testid="password-input"
+          name="password"
+          required
+          type="password"
+        />
+      </label>
+      <button
+        className="login-btn"
+        type="button"
+        data-testid="signin-btn"
+      >
+        Entrar
+      </button>
+      <Link to="./register">
         <button
-          className="login-btn"
-          type="submit"
-          disabled={ password.length <= minPasswordSize }
-          data-testid="signin-btn"
-          onClick={ () => getUserData(email, password) }
+          className="register-btn"
+          type="button"
+          data-testid="no-account-btn"
         >
-          Entrar
+          Ainda não tenho conta
         </button>
-        <Link to="./register">
-          <button
-            className="register-btn"
-            type="button"
-            data-testid="no-account-btn"
-          >
-            Ainda não tenho conta
-          </button>
-        </Link>
-      </form>
+      </Link>
     </div>
   );
 }
--- front-end/src/services/api_endpoints.js
+++ front-end/src/services/api_endpoints.js
@@ -1,12 +1,3 @@
-const axios = require('axios').default;
-
-export const getUserFromAPI = async (email, password) => {
-  const response = await axios.post('http://localhost:3001/login', { email, password })
-    .then((resp) => resp.json())
-    .catch((error) => console.log(error));
-  return response;
-};
-
 // const getUserFromAPI = async (email, pass) => {
 //   const response = await fetch ('http://localhost:3001/login')
 //   .then(response => response.json())
