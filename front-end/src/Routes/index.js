eabed6b2467c62f4bf0b397db890e620f2e99e0b
RobertoAsgon <robertoasgon@gmail.com>
2020-09-24 16:19:40 -0300

    Menu Bar 100% aprovado em teste local.

--- front-end/src/Routes/index.js
+++ front-end/src/Routes/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { register, login, clientProfile } from '../pages';
+import { register, login, clientProfile, products, orders } from '../pages';
 
 const Routes = () => (
   <BrowserRouter>
@@ -9,6 +9,8 @@ const Routes = () => (
       <Route exact path="/login" component={ login } />
       <Route exact path="/register" component={ register } />
       <Route exact path="/profile" component={ clientProfile } />
+      <Route exact path="/products" component={ products } />
+      <Route exact path="/orders" component={ orders } />
     </Switch>
   </BrowserRouter>
 );
