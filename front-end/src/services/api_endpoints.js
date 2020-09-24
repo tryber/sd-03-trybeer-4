72b1570a764ad1427b2b1c7201f821bf35500fe9
luldelrey <rpgstrikered@gmail.com>
2020-09-24 16:11:30 -0300

    corrigindo uma cagada q fiz, apaguei a funcao de registrar usuario no banco :facepalm mas coloquei de volta

--- front-end/src/services/api_endpoints.js
+++ front-end/src/services/api_endpoints.js
@@ -1,8 +1,10 @@
 const axios = require('axios');
 
+const url = 'http://localhost:3001/';
+
 export const getUserFromAPI = async (email, password) => {
   const response = await axios({
-    baseURL: 'http://localhost:3001/login',
+    baseURL: `${url}login`,
     method: 'post',
     data: {
       email,
@@ -14,9 +16,23 @@ export const getUserFromAPI = async (email, password) => {
   return response;
 };
 
+export const postNewUserAPI = async (name, email, password, seller) => {
+  const response = await axios({
+    baseURL: `${url}register`,
+    method: 'post',
+    data: {
+      name,
+      email,
+      password,
+      seller,
+    },
+  })
+    .catch((err) => err);
+};
+
 export const postUpdateName = async (name, email) => {
   const response = await axios({
-    baseURL: 'http://localhost:3001/profile',
+    baseURL: `${url}profile`,
     method: 'post',
     data: {
       name,
