24902be3fbaf559d4e2c43572319aa4db8310776
RobertoAsgon <robertoasgon@gmail.com>
2020-09-28 18:38:22 -0300

    Fix Eslint issues.

--- front-end/src/Components/Checkout/index.js
+++ front-end/src/Components/Checkout/index.js
@@ -1,29 +1,31 @@
-import React, { useState, useEffect, useContext, createFactory } from 'react';
-import MenuBar from '../MenuBar/';
+import React, { useState, useEffect } from 'react';
+import MenuBar from '../MenuBar';
+import { getProductsLocalStorage } from '../../utils/localStorage';
 import './styles.css';
 
-
 const Checkout = () => {
+  // const CartList = JSON.parse(localStorage.getItem('cart')) || [];
   const [cart, setCart] = useState([]);
   const [newCart, setNewCart] = useState([]);
-  const CartList = JSON.parse(localStorage.getItem('cart')) || [];
 
-  const calculePrice = (cart) => cart.reduce((acc, { price, quantity }) => acc + (price * quantity), 0);
+  const calculePrice = (param) => param
+    .reduce((acc, { price, quantity }) => acc + (price * quantity), 1);
 
-  const formatePrice = (price) => price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+  const formatePrice = (price) => price
+    .toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
 
-  const totalPrice = calculePrice(cart).toLocaleString('pt-br',{ style: 'currency', currency: 'BRL' });
+  const totalPrice = calculePrice(cart)
+    .toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
 
   const removeOrder = (index) => {
-    cart.splice(index, 1)
-    setNewCart(cart)
+    cart.splice(index, 1);
+    setNewCart(cart);
     localStorage.setItem('cart', JSON.stringify(cart));
-  }
+  };
 
   useEffect(() => {
-    setCart(CartList)
-  }, [newCart])
-  
+    setCart(getProductsLocalStorage('cart') || []);
+  }, [newCart]);
 
   return (
     <div>
@@ -32,19 +34,19 @@ const Checkout = () => {
       <h2>Produtos</h2>
       {cart.length < 1 && <h2>Não há produtos no carrinho</h2>}
       {cart.map(({ price, productName, quantity }, index) => (
-        <div className="cart-products" key = {index}>
-          <div className="cart-qtd" data-testid={`${index}-product-qtd-input`}>{quantity}</div>
-          <div className="cart-name" data-testid={`${index}-product-name`}>{productName}</div>
-          <div className="cart-total" data-testid={`${index}-product-total-value`}>
-            {formatePrice(quantity * price)}
+        <div className="cart-products" key={ productName }>
+          <div className="cart-qtd" data-testid={ `${index}-product-qtd-input` }>{ quantity }</div>
+          <div className="cart-name" data-testid={ `${index}-product-name` }>{ productName }</div>
+          <div className="cart-total" data-testid={ `${index}-product-total-value` }>
+            { formatePrice(quantity * price) }
           </div>
-          <div className="cart-price" data-testid={`${index}-product-unit-price`}>
-            {`(${formatePrice(price)} un)`} 
+          <div className="cart-price" data-testid={ `${index}-product-unit-price` }>
+            { `(${formatePrice(price)} un)` }
             <button
               type="submit"
               value="Submit"
-              data-testid={`${index}-removal-button`}
-              onClick={() => removeOrder(index)}
+              data-testid={ `${index}-removal-button` }
+              onClick={ () => removeOrder(index) }
             >
               X
             </button>
@@ -53,7 +55,7 @@ const Checkout = () => {
       ))}
 
       <div data-testid="order-total-value">
-        {`Total: ${totalPrice}`}
+        { `Total: ${totalPrice}` }
       </div>
 
       <h2>Endereço</h2>
@@ -65,8 +67,8 @@ const Checkout = () => {
           data-testid="checkout-street-input"
           type="text"
           required
-          // onChange={ (e) => setNewName(e.target.value) && setMessage(null) }
-          // value={ newName }
+        // onChange={ (e) => setNewName(e.target.value) && setMessage(null) }
+        // value={ newName }
         />
       </label>
       <br />
@@ -78,21 +80,21 @@ const Checkout = () => {
           data-testid="checkout-house-number-input"
           type="text"
           required
-          // onChange={ (e) => setNewName(e.target.value) && setMessage(null) }
-          // value={ newName }
+        // onChange={ (e) => setNewName(e.target.value) && setMessage(null) }
+        // value={ newName }
         />
       </label>
       <br />
       <button
         type="button"
         data-testid="checkout-finish-btn"
-        // disabled={ name === newName }
-        // onClick={ () => handleChangeName(name, email) }
+      // disabled={ name === newName }
+      // onClick={ () => handleChangeName(name, email) }
       >
         Finalizar Pedido
       </button>
     </div>
-  )
+  );
 };
 
 export default Checkout;
