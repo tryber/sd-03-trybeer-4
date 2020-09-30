4b71c21e9d829309cf1bc785ff2be689239d3df7
RobertoAsgon <robertoasgon@gmail.com>
2020-09-30 13:46:46 -0300

    Adicionando button desabled.

--- front-end/src/Components/Checkout/index.js
+++ front-end/src/Components/Checkout/index.js
@@ -4,18 +4,28 @@ import { getProductsLocalStorage } from '../../utils/localStorage';
 import './styles.css';
 
 const Checkout = () => {
-  // const CartList = JSON.parse(localStorage.getItem('cart')) || [];
   const [cart, setCart] = useState([]);
   const [newCart, setNewCart] = useState([]);
+  const [nameAdress, setNameAdress] = useState(null);
+  const [numberAdress, setNumberAdress] = useState(null);
+  const zero = 0;
 
-  const calculePrice = (param) => param
-    .reduce((acc, { price, quantity }) => acc + (price * quantity), 1);
+  const calculePrice = (param, paramZero) => param
+    .reduce((acc, { price, quantity }) => acc + (price * quantity), paramZero);
 
   const formatePrice = (price) => price
     .toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
 
-  const totalPrice = calculePrice(cart)
+  const totalPrice = calculePrice(cart, zero)
     .toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+
+  const justNumberPrice = calculePrice(cart, zero);
+
+  const disableButtton = (price, name, number) => {
+    if (price <= zero) return true;
+    if (!name || !number) return true;
+    return false;
+  }
 
   const removeOrder = (index) => {
     cart.splice(index, 1);
@@ -24,16 +34,19 @@ const Checkout = () => {
   };
 
   useEffect(() => {
-    setCart(getProductsLocalStorage('cart') || []);
+    setCart(getProductsLocalStorage('cart'));
   }, [newCart]);
 
+  // console.log('Name', nameAdress);
+  // console.log('Number', numberAdress);
+  // console.log('Total Price', justNumberPrice);
   return (
     <div>
       <MenuBar titleName="Finalizar Pedido" />
 
       <h2>Produtos</h2>
       {cart.length < 1 && <h2>Não há produtos no carrinho</h2>}
-      {cart.map(({ price, productName, quantity }, index) => (
+      {cart.map(({ price = zero, productName, quantity }, index) => (
         <div className="cart-products" key={ productName }>
           <div className="cart-qtd" data-testid={ `${index}-product-qtd-input` }>{ quantity }</div>
           <div className="cart-name" data-testid={ `${index}-product-name` }>{ productName }</div>
@@ -67,8 +80,8 @@ const Checkout = () => {
           data-testid="checkout-street-input"
           type="text"
           required
-        // onChange={ (e) => setNewName(e.target.value) && setMessage(null) }
-        // value={ newName }
+          onChange={ (e) => setNameAdress(e.target.value) }
+          // value={ newName }
         />
       </label>
       <br />
@@ -80,16 +93,16 @@ const Checkout = () => {
           data-testid="checkout-house-number-input"
           type="text"
           required
-        // onChange={ (e) => setNewName(e.target.value) && setMessage(null) }
-        // value={ newName }
+          onChange={ (e) => setNumberAdress(e.target.value) }
+          // value={ newName }
         />
       </label>
       <br />
       <button
         type="button"
         data-testid="checkout-finish-btn"
-      // disabled={ name === newName }
-      // onClick={ () => handleChangeName(name, email) }
+        disabled={ disableButtton(justNumberPrice, nameAdress, numberAdress) }
+        // onClick={ () => handleChangeName(name, email) }
       >
         Finalizar Pedido
       </button>
